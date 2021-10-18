1d5efea6a64b47ba93c67c86c0c8ab5e59819e14
Jasem Valencia <jasemvalencia@gmail.com>
2021-10-18 17:04:17 -0500

    add addElement funtion

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-function addBook(idTitle,idAuthor){    
-   var labelTitle=document.createElement("label");
-   labelTitle.innerText=idTitle.value;
+//Create Constructor
+function Book (title, author){
+    this.title=title;
+    this.author=author;
+};
 
-   var labelAuthor=document.createElement("label");
-   labelAuthor.innerText=idAuthor.value;
+// Create Book Object
+// const book={title:idTitle.value, author:idAuthor.value}
+let bookArr=[];
 
-   var btnRemove=document.createElement("button");
+function addBook(idTitle,idAuthor){   
+    const book=new Book(idTitle.value, idAuthor.value);
+
+    // console.log(book.title);
+
+    book.title=idTitle.value;
+    book.author=idAuthor.value;
+
+    bookArr.push(book);
+    console.log(bookArr);
+
+    var labelTitle=document.createElement("label");
+    labelTitle.innerText=book.title;
+
+    var labelAuthor=document.createElement("label");
+    labelAuthor.innerText=book.author;
+
+    var btnRemove=document.createElement("button");
     btnRemove.innerHTML="Remove";
-   document.body.appendChild(labelTitle);
-   document.body.appendChild(labelAuthor);
 
-   document.body.appendChild(btnRemove)
+    document.body.appendChild(labelTitle);
+    document.body.appendChild(labelAuthor);
+
+    document.body.appendChild(btnRemove)
 }   
+
+ 
 
 
