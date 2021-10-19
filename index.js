195dba7b89578e919d112bf42b46a5b833388c5e
Jasem Valencia <jasemvalencia@gmail.com>
2021-10-19 07:50:42 -0500

    update add funtion

--- index.js
+++ index.js
@@ -1,39 +1,59 @@
 //Create Constructor
-function Book (title, author){
-    this.title=title;
-    this.author=author;
-};
+// function Book (title, author){
+//     this.title=title;
+//     this.author=author;
+// };
 
 // Create Book Object
 // const book={title:idTitle.value, author:idAuthor.value}
-let bookArr=[];
+// let bookArr=[];
 
-function addBook(idTitle,idAuthor){   
-    const book=new Book(idTitle.value, idAuthor.value);
+function addBook(){   
+    //Constructor
+    function Book(title, author){
+        this.title=title;
+        this.author=author;
+    }
+    const titleCapture=document.getElementById("idTitle").value;
+    const authorCapture=document.getElementById("idAuthor").value;
+   
+    objBook=new Book(titleCapture,authorCapture);
+    console.log(objBook);
+    add();
 
-    // console.log(book.title);
+    // console.log(objBook);
 
-    book.title=idTitle.value;
-    book.author=idAuthor.value;
+    // const book=new Book(idTitle.value, idAuthor.value);
 
-    bookArr.push(book);
-    console.log(bookArr);
+    // // console.log(book.title);
 
-    var labelTitle=document.createElement("label");
-    labelTitle.innerText=book.title;
+    // book.title=idTitle.value;
+    // book.author=idAuthor.value;
 
-    var labelAuthor=document.createElement("label");
-    labelAuthor.innerText=book.author;
+    // bookArr.push(book);
+    // console.log(bookArr);
 
-    var btnRemove=document.createElement("button");
-    btnRemove.innerHTML="Remove";
+    // var labelTitle=document.createElement("label");
+    // labelTitle.innerText=book.title;
 
-    document.body.appendChild(labelTitle);
-    document.body.appendChild(labelAuthor);
+    // var labelAuthor=document.createElement("label");
+    // labelAuthor.innerText=book.author;
 
-    document.body.appendChild(btnRemove)
+    // var btnRemove=document.createElement("button");
+    // btnRemove.innerHTML="Remove";
+
+    // document.body.appendChild(labelTitle);
+    // document.body.appendChild(labelAuthor);
+
+    // document.body.appendChild(btnRemove);
 }   
 
+var arrBooks=[];
+function add(){
+     arrBooks.push(objBook);
+     console.log(arrBooks);
+    document.getElementById("table").innerHTML += '<tbody><td>'+objBook.title+'</td> <td>'+objBook.author+'</td></tbody>';
+};
  
 
 
