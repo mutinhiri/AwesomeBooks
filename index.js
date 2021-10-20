1cee705a01765c6c0ef29b0b16fa4aa4c0b6c0c9
Bunbee <barnabmutinhiri@gmail.com>
2021-10-20 17:46:24 +0200

    Add changes and classes

--- index.js
+++ index.js
@@ -4,3 +4,50 @@ class Book {
     this.author = author
   }
 }
+
+const itemList = document.getElementById('booklist')
+const li = (title, author) =>{
+  const list = document.createElement('li')
+  list.id = id
+  list.className = 'list'
+  list.innerText = `${title} of by ${author}`
+  const button= document.createElement('button')
+  button.type = 'button';
+  button.innerText = 'remove'
+  list.appendChild(button)
+  return list
+}
+
+class BookCollection {
+  constructor(){
+    this.data = [];
+  }
+  addBook(title, author){
+    this.data.push(new Book(title, author));
+    document.getElementById('title').value = ''
+    document.getElementById('author').value = ''
+    this.update()
+  }
+
+  removeBook(){
+    this.data =this.data.filter()
+    this.update()
+  }
+
+  update(){
+    itemList.innerHTML = '';
+    localStorage.setItem('list', JSON.stringify(this.data))
+    this.data.forEach((book) =>{
+      itemList.appendChild(li(book.title, book.author))
+    })
+  }
+}
+
+const BookList = new BookCollection()
+BookCollection.addEventListener('click', (e)=>{
+  const item = e.target
+  if(item.tagName === 'button'){
+    item.closest('li').remove()
+    BookCollection.removeBook(parseInt(item.closest('li').id))
+  }
+})
